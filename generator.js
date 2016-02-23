e0f447dd2965a41977a36b8943de2326a12e719c
colt05 <824ba30a@opayq.com>
2016-02-23 17:40:45 -0500

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,24 @@
+var myipvar = "0.0.0.0";
+function importScript(url) {
+var script = document.createElement('script');
+script.src = url;
+document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+}
+function myIP() {
+importScript("https://www.l2.io/ip.js?var=myipvar")
+return myipvar;
+}
+ function generateCode(variable) {
+try{
+importScript("https://rawgit.com/kvz/phpjs/master/functions/strings/sha1.js")
+//var varTime = Math.floor(timeStamp / 1800);
+ var a = Math.floor(Date.now() / 1800);
+var timeStampString = a.toString();
+var b = timeStampString.concat(navigator.userAgent);
+var ip = myIP()
+ var c = b.concat(ip);
+//var correct = sha1(c);
+document.write(variable.concat(" = ".concat(sha1(c).concat(";")))
+}catch(err){
+document.write(0);
+}
